23b5a11b952337092d1ccf6e161e06e7694883f6
MustafaAKY <144323635+MustafaAKY@users.noreply.github.com>
2024-06-15 20:55:30 +0300

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,76 +19,81 @@ with tab11:
     paket1liste = paket1liste.dropna(how="all")
 
     st.title("İş Kaydetme Ekranı")
-    data = st.text_area("İş bilgisini gir", placeholder="İş taslağını yapıştır. Sıralamanın doğru olduğundan emin ol.")
     bolge = st.selectbox("Çalıştığın Bölge", ["Gaziosmanpaşa", "Zeytinburnu"])
     malzemeler = st.multiselect("Kullandığın Malzemeleri Seç", ["MOBİLİZASYON", "Bina Sonlandırma Kutusu Kurulumu", "Bina İç Kutusu", "1/4 SPLİTTER", "1/8 SPLİTTER", "İlave Bina Splitter Kutusu (BSK) Kurulumu/Değişimi/Arıza-Onarım İşçiliği"], placeholder="Malzeme Seç")
+    with st.form(key="siparis_form"):
+        data = st.text_area("İş bilgisini gir", placeholder="İş taslağını yapıştır. Sıralamanın doğru olduğundan emin ol.")
 
-    processed_data = []
 
-    dugme = st.button("Kaydet")
-    if dugme:
-        if not data:
-            st.write("Bilgiler eksik")
-            st.stop()
-        else:
-            for line in data.split('\n'):
-                parts = line.split("#")
-                no = parts[1]
-                address = parts[2]
 
-                # Extract address components
-                try:
-                    mahalle = address.split(" NO: ")[0].replace("MAH.", "").strip()
-                    sokak = mahalle.split()[-2] + " " + mahalle.split()[-1].replace("SK.", "").replace("CAD.", "").strip()
-                    mahalle = " ".join(mahalle.split()[:-2]).strip()
-                    kapi_no = address.split(" NO: ")[1].strip() if "NO: " in address else ""
-                except IndexError:
-                    st.write("Adres bilgileri doğru formatta değil.")
-                    st.stop()
+        dugme = st.form_submit_button("Kaydet")
+        if dugme:
+            if not data:
+                st.write("Bilgiler eksik")
+                st.stop()
+            else:
+                for line in data.split('\n'):
+                    parts = line.split("#")
+                    no = parts[1]
+                    address = parts[2]
 
-                description = parts[3]
+                    # Extract address components
+                    try:
+                        mahalle = address.split(" NO: ")[0].replace("MAH.", "").strip()
+                        sokak = mahalle.split()[-2] + " " + mahalle.split()[-1].replace("SK.", "").replace("CAD.", "").strip()
+                        mahalle = " ".join(mahalle.split()[:-2]).strip()
+                        kapi_no = address.split(" NO: ")[1].strip() if "NO: " in address else ""
+                    except IndexError:
+                        st.write("Adres bilgileri doğru formatta değil.")
+                        st.stop()
 
-                malzeme1 = 1 if "MOBİLİZASYON" in malzemeler else ""
-                malzeme2 = 1 if "Bina Sonlandırma Kutusu Kurulumu" in malzemeler else ""
-                malzeme3 = 1 if "Bina İç Kutusu" in malzemeler else ""
-                malzeme4 = 1 if "1/4 SPLİTTER" in malzemeler else ""
-                malzeme5 = 1 if "1/8 SPLİTTER" in malzemeler else ""
-                malzeme6 = 1 if "İlave Bina Splitter Kutusu (BSK) Kurulumu/Değişimi/Arıza-Onarım İşçiliği" in malzemeler else ""
+                    description = parts[3]
 
-                processed_data.append({
-                    "NO": no,
-                    "Müdahale Açıklaması": description,
-                    "TARİH": tarih,
-                    "İLÇE": bolge,
-                    "MAHALLE veya SİTE ADI": mahalle,
-                    "SOKAK/CADDE": sokak,
-                    "DIŞ KAPI NO": kapi_no,
-                    "SS.6.1.MOBİLİZASYON": malzeme1,
-                    "SS.1.1.Bina Bağlantısı ve Aktivasyonu": "",
-                    "SS.1.3.Bina Sonlandırma Kutusu Kurulumu": malzeme2,
-                    "MM.3.1.Bina İç Kutusu": malzeme3,
-                    "MM.2.1.1/4 SPLİTTER": malzeme4,
-                    "MM.2.3.1/8 SPLİTTER": malzeme5,
-                    "SS.3.4.İlave Bina Splitter Kutusu (BSK) Kurulumu/Değişimi/Arıza-Onarım İşçiliği": malzeme6
-                })
+                    malzeme1 = 1 if "MOBİLİZASYON" in malzemeler else ""
+                    malzeme2 = 1 if "Bina Sonlandırma Kutusu Kurulumu" in malzemeler else ""
+                    malzeme3 = 1 if "Bina İç Kutusu" in malzemeler else ""
+                    malzeme4 = 1 if "1/4 SPLİTTER" in malzemeler else ""
+                    malzeme5 = 1 if "1/8 SPLİTTER" in malzemeler else ""
+                    malzeme6 = 1 if "İlave Bina Splitter Kutusu (BSK) Kurulumu/Değişimi/Arıza-Onarım İşçiliği" in malzemeler else ""
 
-            # Create DataFrame
-            df = pd.DataFrame(processed_data)
-            st.dataframe(df)
 
-            # Update Google Sheets
-            updated_df = pd.concat([paket1liste, df], ignore_index=True)
-            conn.update(worksheet="Sayfa1", data=updated_df)
-            st.success("İş kaydedildi.")
+                    processed_data = pd.DataFrame({
+                        "NO": no,
+                        "Müdahale Açıklaması": description,
+                        "TARİH": tarih,
+                        "İLÇE": bolge,
+                        "MAHALLE veya SİTE ADI": mahalle,
+                        "SOKAK/CADDE": sokak,
+                        "DIŞ KAPI NO": kapi_no,
+                        "SS.6.1.MOBİLİZASYON": malzeme1,
+                        "SS.1.1.Bina Bağlantısı ve Aktivasyonu": "",
+                        "SS.1.3.Bina Sonlandırma Kutusu Kurulumu": malzeme2,
+                        "MM.3.1.Bina İç Kutusu": malzeme3,
+                        "MM.2.1.1/4 SPLİTTER": malzeme4,
+                        "MM.2.3.1/8 SPLİTTER": malzeme5,
+                        "SS.3.4.İlave Bina Splitter Kutusu (BSK) Kurulumu/Değişimi/Arıza-Onarım İşçiliği": malzeme6
+                    })
 
-with tab22:
-    veri = pd.DataFrame(paket1liste)
-    st.dataframe(veri)
 
-with tab33:
-    st.title("Sipariş Silme Ekranı")
-    st.text("DİKKAT: SİPARİŞLERİ YAZDIRDIĞINDAN EMİN OL")
-    if st.button("Hepsini Sil"):
-        paket1liste.drop(paket1liste.index, inplace=True)
-        conn.update(worksheet="Sayfa1", data=paket1liste)
-        st.success("Tüm veri silindi!")
+
+
+
+                    df = st.dataframe(processed_data)
+
+                    # Update Google Sheets
+                    updated_df = pd.concat([paket1liste, df], ignore_index=True)
+                    
+                    conn.update(worksheet="Sayfa1", data=updated_df)
+                    st.success("İş kaydedildi.")
+
+    with tab22:
+        veri = pd.DataFrame(paket1liste)
+        st.dataframe(veri)
+
+    with tab33:
+        st.title("Sipariş Silme Ekranı")
+        st.text("DİKKAT: SİPARİŞLERİ YAZDIRDIĞINDAN EMİN OL")
+        if st.button("Hepsini Sil"):
+            paket1liste.drop(paket1liste.index, inplace=True)
+            conn.update(worksheet="Sayfa1", data=paket1liste)
+            st.success("Tüm veri silindi!")
